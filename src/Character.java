0504d3aa6e800f15a4f0c409161ebc41d3824145
Benjamin Rojas-Ritchie <ben@benarr.com>
2022-07-25 16:16:25 -0600

    Character inventory methods

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Character {
     String characterName = "";
     int characterHealth = 50;
@@ -6,6 +8,7 @@ public class Character {
     int special;
     String armor = "";
     String[] inventory = new String[9];
+    Scanner scnr = new Scanner(System.in);
 
     // Need to add way to add/remove from inventory
 
@@ -19,6 +22,43 @@ public class Character {
 
     public int heal (int hitPoints) {
         return characterHealth + hitPoints;
+    }
+
+    public boolean checkInventorySpace() {
+        for (String s : inventory) {
+            if (s == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int removeFromInventory() {
+        System.out.println("Please choose an item to replace in your inventory:");
+        for (int i = 0; i < inventory.length; ++i) {
+            System.out.println(i + inventory[i]);
+        }
+        return scnr.nextInt();
+    }
+
+    public String[] modifyInventory (String add) {
+        String[] newInventory = new String[9];
+        if (checkInventorySpace()) {
+            for (int i = 0; i < inventory.length; ++i) {
+                if (inventory[i] == null) {
+                    inventory[i] = add;
+                    break;
+                }
+                else {
+                    newInventory[i] = inventory[i];
+                }
+            }
+        }
+        else {
+            int index = removeFromInventory();
+            inventory[index] = add;
+        }
+        return newInventory;
     }
 }
 
