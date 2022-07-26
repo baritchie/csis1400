143d67eab5b4ef198881b4dba3ce63025f5eaf7b
Benjamin Rojas-Ritchie <ben@benarr.com>
2022-07-25 21:44:28 -0600

    Encounters object return

--- src/Encounter.java
+++ src/Encounter.java
@@ -20,20 +20,20 @@ class Encounters {
         this.coinsOnDeath = coinsOnDeath;
     }
 
-    public static void main(String[] args) {
+    public Encounters[] setEncounters() {
         Encounters[] encounter = new Encounters[5];
         encounter[0] = new Encounters("Goblins", 3, 8, 10);
         encounter[1] = new Encounters("a Lich", 5, 10, 15);
         encounter[2] = new Encounters("a band of Knight", 5, 9, 17);
         encounter[3] = new Encounters("a group of the Undead", 2, 6, 10);
         encounter[4] = new Encounters("the Leshy", 8, 15, 25);
+        return encounter;
     }
-    public void currEncounter(int num) {
 
+    public void currEncounter(int num) {
+        Encounters[] encounter = setEncounters();
         Scanner scnr = new Scanner(System.in);
         System.out.println("Oh no! You've run into" + encounter[num]);
-
-
         scnr.close();
     }
 }
