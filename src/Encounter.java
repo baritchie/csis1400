c830c6880d256eb3802ee2982a3c26169e2d57ae
Zach Royer <zach.royer14@gmail.com>
2022-07-25 21:22:49 -0600

    Updates to Encounter

--- src/Encounter.java
+++ src/Encounter.java
@@ -30,10 +30,7 @@ class Encounters {
     public void currEncounter(int num) {
 
         Scanner scnr = new Scanner(System.in);
-        String[] encounterName = new String[] { "Goblins", "a Lich", "A Band of Knights", "the Undead" }; // An Array containing encounter names
-        int[] encounterLightDamage = new int[] { 3, 5, 4, 2 }; // An Array containing light damage as a result of making a mistake
-        int[] encounterFullDamage = new int[] { 8, 10, 9, 7 }; // An Array containing the full damage as a result of failing the encounter
-        System.out.println("Oh no! You've run into" + encounterName[num]);
+        System.out.println("Oh no! You've run into" + encounter[num]);
 
 
         scnr.close();
